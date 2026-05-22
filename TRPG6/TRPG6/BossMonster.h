26b1fe6fbdff17a912b29e991e12c4176d390925
siha37 <48675170+siha37@users.noreply.github.com>
2026-05-22 16:39:21 +0900

    Add: BossMonster 클래스 추가
    생성자 추가

---
+++ TRPG6/TRPG6/BossMonster.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Monster.h"
+
+class BossMonster : public Monster
+{
+private:
+    static const std::string BossName;
+public:
+    BossMonster(int playerLevel = 1) : Monster(playerLevel, BossName,1.5f) {}
+
+    std::string WhoAmI() override;
+    virtual Monster* ResetState(int playerLevel) override;
+};
+
