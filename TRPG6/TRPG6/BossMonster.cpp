632fd2e5f72acd78543d14fe05db38db3703e067
siha37 <48675170+siha37@users.noreply.github.com>
2026-05-22 17:10:41 +0900

    fix: bossMonster ResetState 수정
    - 기존 스탯 배율이 적용 안되는 문제 수정
    - 이름 반환 함수의 리턴 처리 수정

--- TRPG6/TRPG6/BossMonster.cpp
+++ TRPG6/TRPG6/BossMonster.cpp
@@ -4,14 +4,14 @@ const std::string BossMonster::BossName = "대래래래래곤~~~";
 
 std::string BossMonster::WhoAmI()
 {
-    return std::string();
+    return Name;
 }
 
 Monster* BossMonster::ResetState(int playerLevel) 
 {
     Name = BossName;
-    Hp = RandomHp(playerLevel);
-    Attack = RandomAttack(playerLevel);
-    Money = RandomMoney(playerLevel);
+    Hp = RandomHp(playerLevel,1.5f);
+    Attack = RandomAttack(playerLevel, 1.5f);
+    Money = RandomMoney(playerLevel, 1.5f);
     return this;
 }
