d06da623e43aa86c8207ab10b2dce8f045ef2dd6
NDH <ndhphysics@gmail.com>
2026-05-26 19:00:31 +0900

    add: 아레나 게임 상태 뼈대 코드 추가

---
+++ TRPG6/TRPG6/ArenaReadyState.cpp
@@ -0,0 +1,20 @@
+﻿#include "ArenaReadyState.h"
+#include "ArenaLobbyState.h"
+#include "GameManager.h"
+#include "Renderer.h"
+
+void ArenaReadyState::Enter() {
+    Renderer::ClearAllCenterLeftUI();
+}
+
+void ArenaReadyState::Update(int ch, std::string& lastCommand) {
+    Renderer::DisplayUI(UIPart::Top, 0, "아레나 준비 중");
+    Renderer::DisplayUI(UIPart::CenterLeft, 9, "1. 아레나 로비 입장");
+
+    if (ch == 1) {
+        GameManager::GetInstance().SetCurrentState(new ArenaLobbyState());
+    }
+}
+
+void ArenaReadyState::Exit() {
+}
---
+++ TRPG6/TRPG6/ArenaWaitState.cpp
@@ -0,0 +1,13 @@
+﻿#include "ArenaWaitState.h"
+#include "Renderer.h"
+
+void ArenaWaitState::Enter() {
+    Renderer::ClearAllCenterLeftUI();
+}
+
+void ArenaWaitState::Update(int ch, std::string& lastCommand) {
+    Renderer::DisplayUI(UIPart::Top, 0, "아레나 대기 중...");
+}
+
+void ArenaWaitState::Exit() {
+}
