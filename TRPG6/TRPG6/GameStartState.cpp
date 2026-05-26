4ed16a89bbb96e152d5c3bd7d538d95eac70f13c
RheaGums <dhwlsals99@gmail.com>
2026-05-26 16:44:13 +0900

    feat: 킬로그 출력 기능추가, 인벤토리 출력부 변경
    feat: 킬로그 출력 기능추가, 인벤토리 출력부 변경

--- TRPG6/TRPG6/GameStartState.cpp
+++ TRPG6/TRPG6/GameStartState.cpp
@@ -11,6 +11,7 @@
 #include "ShopStockState.h"
 #include "IPCManager.h"
 #include "Utils.h" //아스키 아트를 위한 include
+#include "InventoryState.h" // 인벤토리
 
 void GameStartState::Enter()
 {
@@ -33,9 +34,10 @@ void GameStartState::Update(int ch, std::string& lastCommand) {
     // 5. 메뉴 출력 + switch
 
     Renderer::DisplayUI(UIPart::Top, 0, "메인 화면");
-    Renderer::DisplayUI(UIPart::CenterLeft, 10, "1. 던전 입장");
-    Renderer::DisplayUI(UIPart::CenterLeft, 11, "2. 상점 입장");
-    Renderer::DisplayUI(UIPart::CenterLeft, 12, "3. 인벤토리 확인");
+    Renderer::DisplayUI(UIPart::CenterLeft, 9, "1. 던전 입장");
+    Renderer::DisplayUI(UIPart::CenterLeft, 10, "2. 상점 입장");
+    Renderer::DisplayUI(UIPart::CenterLeft, 11, "3. 인벤토리 확인");
+    Renderer::DisplayUI(UIPart::CenterLeft, 12, "4. 킬로그 확인");
     switch (ch) {
     case 1: {
         GameManager::GetInstance().SetCurrentState(new BattleState());
@@ -51,28 +53,11 @@ void GameStartState::Update(int ch, std::string& lastCommand) {
         break;
 
     case 3:
-        Player * player = GameManager::GetInstance().GetPlayer();
-        auto& inventory = player->GetInventory();
-        const auto& slots = inventory.GetSlots();
-        int size = (int)slots.size();
-
-        if (size == 0)
-        {
-            Renderer::DisplayUI(UIPart::CenterLeft, 3, "인벤토리가 비어있습니다.");
-        }
-        else
-        {
-            for (int i = 0; i < size; ++i)
-            {
-                std::string name = slots[i].item->GetName();
-                int count = slots[i].count;
-                int sellPrice = slots[i].item->GetSellPrice();
-                std::string info = std::to_string(i + 1) + ". " + name + " (x" + std::to_string(count) + ") | 가격: " + std::to_string(sellPrice) + "G";
-                Renderer::DisplayUI(UIPart::CenterLeft, i + 3, info);
-            }
-        }
+        GameManager::GetInstance().SetCurrentState(new InventoryState());
         break;
 
-
+    case 4:
+        GameManager::GetInstance().GetBattleManager()->GetAllKillCount();
+        break;
     }
 }
