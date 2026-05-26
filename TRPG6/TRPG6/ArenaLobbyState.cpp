e45dd3581073d601424215c11f6e98e5f6b285c9
RheaGums <dhwlsals99@gmail.com>
2026-05-26 20:59:20 +0900

    feat: ArenaLobbyState Ready 패킷 송수신 구현
    - NetworkManager에 SendArenaReady() 추가
    - PKT_C2S_ARENA_READY 패킷 처리 추가
    - PKT_S2C_CHANGE_STATE ArenaBattle 전환 추가
    - ArenaLobbyState Enter()에서 SendArenaReady() 호출

--- TRPG6/TRPG6/ArenaLobbyState.cpp
+++ TRPG6/TRPG6/ArenaLobbyState.cpp
@@ -2,16 +2,18 @@
 #include "ArenaBattleState.h"
 #include "GameManager.h"
 #include "Renderer.h"
+#include "NetworkManager.h"
 
 void ArenaLobbyState::Enter() {
     Renderer::ClearAllCenterLeftUI();
+    NetworkManager::GetInstance().SendArenaReady();
 }
 
 void ArenaLobbyState::Update(int ch, std::string& lastCommand) {
     Renderer::DisplayUI(UIPart::Top, 0, "아레나 로비");
     Renderer::DisplayUI(UIPart::CenterLeft, 9, "1. 전투 시작");
 
-    if (ch == 1) {
+    if (ch == 1 && Client::isServer) {
         GameManager::GetInstance().SetCurrentState(new ArenaBattleState());
     }
 }
--- TRPG6/TRPG6/ArenaLobbyState.h
+++ TRPG6/TRPG6/ArenaLobbyState.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include "IGameState.h"
 
 class ArenaLobbyState : public IGameState {
