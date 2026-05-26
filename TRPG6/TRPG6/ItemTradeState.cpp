90f3b83c375fdcdb29c8daeb2d25e911c3959d89
goeun441 <120786130+Jang-Goeun@users.noreply.github.com>
2026-05-26 23:11:59 +0900

    add: ItemTradeState 추가
    아이템 거래 센터

---
+++ TRPG6/TRPG6/ItemTradeState.cpp
@@ -0,0 +1,47 @@
+﻿#include "ItemTradeState.h"
+#include "Renderer.h"
+#include "GameManager.h"
+#include "GameStartState.h"
+#include "ItemTradeRequestState.h"
+#include "ItemSentTradeListState.h"
+#include "ItemReceivedTradeListState.h"
+
+void ItemTradeState::Enter()
+{
+}
+
+void ItemTradeState::Update(int ch, std::string & lastCommand)
+{
+    Renderer::ClearAllCenterLeftUI();
+    player = GameManager::GetInstance().GetPlayer();
+
+    if (player != nullptr)
+    {
+        player->PrintStatus();
+    }
+
+    Renderer::DisplayUI(UIPart::CenterLeft, 0, "=== [ 아이템 거래 센터 ] ===");
+    Renderer::DisplayUI(UIPart::CenterLeft, 2, "1. 거래 신청하기 (신규 제안)");
+    Renderer::DisplayUI(UIPart::CenterLeft, 3, "2. 내가 보낸 거래 목록 보기");
+    Renderer::DisplayUI(UIPart::CenterLeft, 4, "3. 나에게 온 거래 목록 보기");
+    Renderer::DisplayUI(UIPart::CenterLeft, 6, "0. 메인 화면으로 돌아가기");
+
+    // 입력 처리
+    switch (ch)
+    {
+        case 0:
+            GameManager::GetInstance().SetCurrentState(new GameStartState());
+            break;
+        case 1:
+            GameManager::GetInstance().SetCurrentState(new ItemTradeRequestState());
+            break;
+        case 2:
+            GameManager::GetInstance().SetCurrentState(new ItemSentTradeListState());
+            break;
+        case 3:
+            GameManager::GetInstance().SetCurrentState(new ItemReceivedTradeListState());
+            break;
+        default:
+            break;
+    }
+}
