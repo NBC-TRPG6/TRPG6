b333253b9876301ebaa75e0ef56d98f4a63008bd
goeun441 <120786130+Jang-Goeun@users.noreply.github.com>
2026-05-26 12:45:55 +0900

    add: ShopStockState클래스 추가
    상점 재고 목록 출력을 위한 State 추가함

---
+++ TRPG6/TRPG6/ShopStockState.cpp
@@ -0,0 +1,35 @@
+﻿#include "ShopStockState.h"
+#include "GameManager.h"
+#include "ShopState.h"
+#include "Renderer.h"
+#include "Player.h"
+#include "Shop.h"
+#include "Utils.h"
+
+void ShopStockState::Enter()
+{
+    auto art = LoadImageAsASCII("..\\..\\Resources\\nemo.png");
+    Renderer::SetTopASCIIImage(art);
+}
+
+void ShopStockState::Update(int ch, std::string& lastCommand)
+{
+    player = GameManager::GetInstance().GetPlayer();
+
+    if (player != nullptr)
+    {
+        player->PrintStatus();
+    }
+
+    Renderer::DisplayUI(UIPart::CenterLeft, 0, "==== [ 상점 재고 목록 ] ====");
+    
+    Shop::GetInstance().ShowStock();
+
+    int stockSize = Shop::GetInstance().GetStockSize();
+    Renderer::DisplayUI(UIPart::CenterLeft, stockSize + 2, "0. 이전으로 돌아가기");
+
+    if (ch == 0)
+    {
+        GameManager::GetInstance().SetCurrentState(new ShopState());
+    }
+}
