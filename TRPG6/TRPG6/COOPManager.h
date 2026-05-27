69fde3ed3a533573c927a0ddc96a4bdf297028da
NDH <ndhphysics@gmail.com>
2026-05-27 15:27:22 +0900

    add: 레이드용 데이터 관리를 위한 싱글턴 COOPManager 추가

---
+++ TRPG6/TRPG6/COOPManager.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+#include <string>
+#include <map>
+#include <vector>
+#include "Packet.h"
+
+struct COOPPlayerInfo {
+    std::string name;
+    int atk;
+    int hp;
+    PlayerJob job;
+    bool isDead;
+    bool isReady;
+};
+
+class COOPManager {
+public:
+    static COOPManager& GetInstance() {
+        static COOPManager instance;
+        return instance;
+    }
+
+    void Reset();
+
+    // C2S processing
+    void SetPlayerReady(const std::string& name, bool isReady);
+    void UpdatePlayerStatus(const std::string& name, int atk, int hp, PlayerJob job, bool isDead);
+    void OnPlayerAttack(const std::string& sourceName, const std::string& targetName, int amount);
+    void OnPlayerBlock(const std::string& sourceName, const std::string& targetName);
+    void OnPlayerHeal(const std::string& sourceName, const std::string& targetName, int amount);
+    void OnPlayerItem(const std::string& targetName, const std::string& itemName, int amount);
+
+    // S2C processing (for clients)
+    void UpdateTurn(const std::string& targetName, int turn);
+    void UpdateMonster(const std::string& targetName, int hp);
+    void TakeItem(const std::string& targetName, const std::string& itemName);
+
+    // Helpers
+    bool IsMyTurn() const;
+    PlayerJob GetMyJob() const;
+
+    std::map<std::string, COOPPlayerInfo> players;
+    std::string currentBossName;
+    int currentBossHp;
+    std::string currentTurnPlayer;
+    int currentTurnCount;
+    std::string currentBlockSource;
+    std::string currentBlockTarget;
+    std::vector<std::string> turnOrder;
+
+    void NextTurn();
+
+private:
+    COOPManager() = default;
+    ~COOPManager() = default;
+    
+    void CheckAllReady();
+    void BossAction();
+};
